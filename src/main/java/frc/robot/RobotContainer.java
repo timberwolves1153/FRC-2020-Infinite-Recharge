1876bc270778acc4adc3a95f5342ed2deba15047
Kyle Schmottlach <kyle106@me.com>
2020-02-13 22:35:54 -0500

    Disabled LiveWindow

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,6 +9,7 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -89,6 +90,8 @@ public class RobotContainer {
     SmartDashboard.putData("Auto Selector", chooseAutoCommand);
 
     configureButtonBindings();
+
+    LiveWindow.disableAllTelemetry();
 
     drive.setDefaultCommand(new DefaultDrive(drive,
         () -> driver.getRawAxis(1),
